f5b3fd77dc8a2cee0a8dd9f5ca05e07cbcecd4cf
Robert Pack <42610831+roeap@users.noreply.github.com>
2022-02-09 21:33:26 +0100

    Execute client requests on py thread (#107)
    * update readme
    
    * block on python call

--- python/flight-fusion/src/utils.rs
+++ python/flight-fusion/src/utils.rs
@@ -3,6 +3,7 @@ use std::future::Future;
 use tokio::runtime::Runtime;
 
 /// Utility to collect rust futures with GIL released
+#[allow(dead_code)]
 pub(crate) fn wait_for_future<F: Future>(py: Python, f: F) -> F::Output
 where
     F: Send,
@@ -10,4 +11,13 @@ where
 {
     let rt = Runtime::new().unwrap();
     py.allow_threads(|| rt.block_on(f))
+}
+
+pub(crate) fn wait_for_future_blocking<F: Future>(f: F) -> F::Output
+where
+    F: Send,
+    F::Output: Send,
+{
+    let rt = Runtime::new().unwrap();
+    rt.block_on(f)
 }
