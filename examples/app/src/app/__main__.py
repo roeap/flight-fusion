f88d0785791011d4f39485e7596155beb26e1839
Robert Pack <robstar.pack@gmail.com>
2022-04-17 10:18:45 +0200

    fix dependency issue

---
+++ examples/app/src/app/__main__.py
@@ -0,0 +1,4 @@
+from .index import app
+
+if __name__ == "__main__":
+    app.run_server(port="8888", debug=True)
--- examples/app/src/app/index.py
+++ examples/app/src/app/index.py
@@ -95,7 +95,3 @@ def render_page_content(pathname):
         ),
         className="p-3 bg-light rounded-3",
     )
-
-
-if __name__ == "__main__":
-    app.run_server(port="8888", debug=True)
