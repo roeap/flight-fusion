8a880413103f7e428392a1ad46acfbd4c0d22058
Robert Pack <robstar.pack@gmail.com>
2022-04-25 00:05:48 +0200

    add additional auth option

--- python/arrow-adx/arrow_adx/_internal.pyi
+++ python/arrow-adx/arrow_adx/_internal.pyi
@@ -10,6 +10,22 @@ class KustoClient:
     def __init__(self, service_url: str) -> None:
         """Create a new instance of a KustoClient"""
         ...
+    @classmethod
+    def with_aad_application_key_authentication(
+        cls, connection_string: str, aad_app_id: str, app_key: str, authority_id: str
+    ) -> KustoClient:
+        """Creates a KustoConnection string builder that will authenticate with AAD application and key.
+
+        Args:
+            connection_string: Kusto connection string should be of the format: https://<clusterName>.kusto.windows.net
+            aad_app_id: AAD application ID.
+            app_key: Corresponding key of the AAD application.
+            authority_id: Authority id (aka Tenant id) must be provided
+
+        Returns:
+            KustoClient: The client instance
+        """
+        ...
     def execute(self, database: str, query: str) -> list[pa.RecordBatch]:
         """_summary_
 
