bff30353aea1ce7a7f1672cdcca4d7f807e78191
Robert Pack <robstar.pack@gmail.com>
2022-05-15 09:22:21 +0200

    move arrow-adx to separate repo

--- python/arrow-adx/arrow_adx/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa 405
-from . import _internal
-from ._internal import KustoClient, rust_core_version
-from .version import __version__
-
-__doc__ = _internal.__doc__
-
-__all__ = ("rust_core_version", "KustoClient")
