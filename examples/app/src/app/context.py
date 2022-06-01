385ad03149eeb2402d97a3a01a71c57e19a67b75
Robert Pack <robstar.pack@gmail.com>
2022-06-01 12:09:42 +0200

    hello plotting

---
+++ examples/app/src/app/context.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import json
+from typing import TYPE_CHECKING
+
+import flask
+from dash._callback_context import CallbackContext as _CallbackContext
+from dash._callback_context import has_context
+
+from flight_fusion import (
+    AssetKey,
+    BaseDatasetClient,
+    ClientOptions,
+    FusionServiceClient,
+)
+
+if TYPE_CHECKING:
+    import pyarrow as pa
+
+
+def _assert_asset_key(asset_key: AssetKey | list[str] | str) -> AssetKey:
+    if isinstance(asset_key, AssetKey):
+        return asset_key
+    if isinstance(asset_key, str):
+        return AssetKey(json.loads(asset_key))
+    return AssetKey(asset_key)
+
+
+class DashAssetClient:
+    def __init__(self, client: BaseDatasetClient) -> None:
+        self._client = client
+
+    @property
+    def client(self) -> BaseDatasetClient:
+        return self._client
+
+    def column_options(self):
+        return [{"label": f.name, "value": f.name} for f in self.client.schema()]
+
+    def load_columns(self, columns: list[str]) -> pa.Table:
+        return self.client.load(columns=columns)
+
+
+class CallbackContext(_CallbackContext):
+    @property
+    @has_context
+    def fusion(self) -> FusionServiceClient:
+        if not hasattr(flask.g, "_fusion"):
+            ffc = FusionServiceClient(ClientOptions(host="localhost", port=50051))
+            setattr(flask.g, "_fusion", ffc)
+        return getattr(flask.g, "_fusion")
+
+    @has_context
+    def get_dataset_client(
+        self, asset_key: AssetKey | list[str] | str
+    ) -> DashAssetClient:
+        asset_key = _assert_asset_key(asset_key)
+        return DashAssetClient(self.fusion.get_dataset_client(asset_key=asset_key))
+
+    @has_context
+    def get_versioned_dataset_client(
+        self, asset_key: AssetKey | list[str] | str
+    ) -> DashAssetClient:
+        asset_key = _assert_asset_key(asset_key)
+        return DashAssetClient(
+            self.fusion.get_versioned_dataset_client(asset_key=asset_key)
+        )
+
+
+ctx = CallbackContext()
--- examples/app/src/app/pages/01_datasets.py
+++ examples/app/src/app/pages/01_datasets.py
@@ -1,34 +1,105 @@
-import dash_antd as dadc
-import plotly.graph_objects as go
-from dash import dcc, register_page  # type: ignore
+import json
 
+import dash_antd as dadc
+import plotly.express as px
+import plotly.graph_objects as go
+from dash import register_page  # type: ignore
+from dash import Input, Output, State, callback, dcc, no_update
+
+from app.context import ctx
 from app.themes import theme_light
 
 register_page(path="/", title="Explorer", icon="ControlOutlined")  # type: ignore
 
-layout = [
-    dadc.Row(
-        dadc.PageHeader(
-            [
-                dadc.Input(),
-                dadc.PageHeaderOperation(dadc.Button("Update Plot", type="primary")),
-            ],
-            title="Dataset",
-            ghost=False,
-            style={
-                "width": "100%",
-                "height": 150,
-                "borderBottom": "1px solid rgba(0,0,0,.06)",
-            },
+
+def get_layout(dataset_options):
+    return [
+        dadc.Row(
+            dadc.PageHeader(
+                [
+                    dadc.Space(
+                        [
+                            dadc.Select(
+                                id="dataset",
+                                options=dataset_options,
+                                style={"width": 250},
+                            ),
+                            dadc.Select(
+                                id="columns-x",
+                                options=[],
+                                style={"width": 250},
+                            ),
+                            dadc.Select(
+                                id="columns-y",
+                                options=[],
+                                style={"width": 250},
+                            ),
+                        ]
+                    ),
+                    dadc.PageHeaderOperation(
+                        dadc.Button("Update Plot", id="update-plot", type="primary")
+                    ),
+                ],
+                title="Dataset",
+                ghost=False,
+                style={
+                    "width": "100%",
+                    "height": 150,
+                    "borderBottom": "1px solid rgba(0,0,0,.06)",
+                },
+            ),
+            style={"height": "150px", "width": "100%"},
         ),
-        style={"height": "150px", "width": "100%"},
-    ),
-    dadc.Row(
-        dcc.Graph(
-            figure=go.Figure(layout={"template": theme_light}),
-            responsive=True,
+        dadc.Row(
+            dcc.Graph(
+                id="dataset-plot",
+                figure=go.Figure(layout={"template": theme_light}),
+                responsive=True,
+                style={"height": "calc(100vh - 200px)", "width": "100%"},
+            ),
             style={"height": "calc(100vh - 200px)", "width": "100%"},
         ),
-        style={"height": "calc(100vh - 200px)", "width": "100%"},
-    ),
-]
+    ]
+
+
+def layout():
+    dataset_options = [
+        {
+            "label": ">".join(info.asset_key.path),
+            "value": json.dumps(info.asset_key.path),
+        }
+        for info in ctx.fusion.list_datasets()
+    ]
+    return get_layout(dataset_options)
+
+
+@callback(
+    Output("columns-x", "options"),
+    Output("columns-y", "options"),
+    Output("columns-x", "value"),
+    Output("columns-y", "value"),
+    Input("dataset", "value"),
+    prevent_initial_call=True,
+)
+def update_selects(asset_key: str):
+    asset = ctx.get_dataset_client(asset_key)
+    options = asset.column_options()
+    return options, options, None, None
+
+
+@callback(
+    Output("dataset-plot", "figure"),
+    Input("update-plot", "n_clicks"),
+    State("dataset", "value"),
+    State("columns-x", "value"),
+    State("columns-y", "value"),
+    prevent_initial_call=True,
+)
+def update_plot(
+    n_clicks: int | None, asset_key: str | None, x_col: str | None, y_col: str | None
+):
+    if x_col is None or y_col is None or asset_key is None:
+        return no_update
+    asset = ctx.get_dataset_client(asset_key)
+    data = asset.load_columns(columns=[x_col, y_col]).to_pandas()
+    return px.scatter(data, x=x_col, y=y_col, template=theme_light)
