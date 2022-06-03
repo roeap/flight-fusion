736415be5086b095be1f13c6a94483ec5f0c7809
Robert Pack <robstar.pack@gmail.com>
2022-06-03 16:57:38 +0200

    skeleton artifact repo

--- python/mlflow-fusion/README.md
+++ python/mlflow-fusion/README.md
@@ -1,1 +1,9 @@
 # mlflow-fusion
+
+- artifact store plugin
+
+save and load model artifacts
+
+- model evaluator plugin
+
+evaluate models in deployed apps
---
+++ python/mlflow-fusion/mlflow_fusion/artifacts.py
@@ -0,0 +1,53 @@
+# from mlflow.exceptions import MlflowException
+from mlflow.store.artifact.artifact_repo import ArtifactRepository
+
+
+class FusionArtifactRepository(ArtifactRepository):
+    def __init__(self, artifact_uri):
+        super().__init__(artifact_uri)
+        self.artifact_uri = artifact_uri
+
+    def log_artifact(self, local_file, artifact_path=None):
+        """
+        Log a local file as an artifact, optionally taking an ``artifact_path`` to place it in
+        within the run's artifacts. Run artifacts can be organized into directories, so you can
+        place the artifact in a directory this way.
+
+        :param local_file: Path to artifact to log
+        :param artifact_path: Directory within the run's artifact directory in which to log the
+                              artifact.
+        """
+        pass
+
+    def log_artifacts(self, local_dir, artifact_path=None):
+        """
+        Log the files in the specified local directory as artifacts, optionally taking
+        an ``artifact_path`` to place them in within the run's artifacts.
+
+        :param local_dir: Directory of local artifacts to log
+        :param artifact_path: Directory within the run's artifact directory in which to log the
+                              artifacts
+        """
+        pass
+
+    def list_artifacts(self, path):
+        """
+        Return all the artifacts for this run_id directly under path. If path is a file, returns
+        an empty list. Will error if path is neither a file nor directory.
+
+        :param path: Relative source path that contains desired artifacts
+
+        :return: List of artifacts as FileInfo listed directly under path.
+        """
+        pass
+
+    def _download_file(self, remote_file_path, local_path):
+        """
+        Download the file at the specified relative remote path and saves
+        it at the specified local path.
+
+        :param remote_file_path: Source path to the remote file, relative to the root
+                                 directory of the artifact repository.
+        :param local_path: The path to which to save the downloaded file.
+        """
+        pass
