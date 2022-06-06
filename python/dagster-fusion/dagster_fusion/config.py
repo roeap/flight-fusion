5ced1680e80b558fc3502cf3eb35548cc5818173
Robert Pack <robstar.pack@gmail.com>
2022-06-06 02:05:56 +0200

    config cleanup

--- python/dagster-fusion/dagster_fusion/config.py
+++ python/dagster-fusion/dagster_fusion/config.py
@@ -1,34 +1,7 @@
 from __future__ import annotations
 
-from dagster import Array, AssetKey, Enum, Field, Selector, Shape, String
-from flight_fusion.ipc.v1alpha1 import AreaSourceReference, SaveMode
-
-from dagster_fusion.errors import MissingConfiguration
-
-_KEY_SEPARATOR = "/"
-
-FIELD_LOCATION = Field(
-    Selector(
-        {
-            "key": Field(String, is_required=False),
-            # TODO find a better name then `source`
-            "source": Field(
-                Shape(
-                    fields={
-                        "name": Field(
-                            String,
-                            is_required=True,
-                            description="Table / location name where data is loaded from",
-                        ),
-                        "areas": Field(Array(String)),
-                    }
-                ),
-                is_required=False,
-            ),
-        }
-    ),
-    is_required=False,
-)
+from dagster import Array, Enum, Field, String
+from flight_fusion.ipc.v1alpha1 import SaveMode
 
 FIELD_COLUMN_SELECTION = Field(
     Array(String),
@@ -42,9 +15,3 @@ FIELD_SAVE_MODE = Field(
     default_value="SAVE_MODE_APPEND",
     description="Specifies behavior when saving data into a table location",
 )
-
-
-def area_source_to_asset_key(reference: AreaSourceReference) -> AssetKey:
-    if not reference.location:
-        raise MissingConfiguration("Either location 'source' or 'key' must be configured")
-    return AssetKey(reference.location.areas + [reference.location.name])
