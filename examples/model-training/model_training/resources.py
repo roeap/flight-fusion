67bf7238057069caaca4354b6abe007b3825000b
Robert Pack <robstar.pack@gmail.com>
2022-06-06 08:28:42 +0200

    cleanup model example

--- examples/model-training/model_training/resources.py
+++ examples/model-training/model_training/resources.py
@@ -1,16 +1,18 @@
+from dagster import make_values_resource
 from dagster_mlflow import mlflow_tracking
 
 from dagster_fusion import (
+    flight_fusion_io_manager,
     flight_fusion_resource,
     mlfusion_configuration,
-    mlfusion_io_manager,
 )
 
-from .dataset_spec import dataset_properties
+dataset_properties = make_values_resource(n_samples=int, test_split=float)
+
 
 RESOURCES_LOCAL = {
     "mlfusion_config": mlfusion_configuration,
-    "fusion_io_manager": mlfusion_io_manager,
+    "fusion_io_manager": flight_fusion_io_manager,
     "fusion_client": flight_fusion_resource,
     "dataset_properties": dataset_properties.configured(
         {
@@ -18,7 +20,5 @@ RESOURCES_LOCAL = {
             "test_split": 0.3,
         }
     ),
-    "mlflow": mlflow_tracking.configured(
-        {"experiment_name": "demo_experiment", "mlflow_tracking_uri": "http://localhost:5000"}
-    ),
+    "mlflow": mlflow_tracking.configured({"experiment_name": "demo_experiment"}),
 }
--- examples/model-training/model_training/resources/dataset_spec.py
+++
@@ -1,3 +0,0 @@
-from dagster import make_values_resource
-
-dataset_properties = make_values_resource(n_samples=int, test_split=float)
